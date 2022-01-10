33e15f144534d39d8c2896b263f1f4f2a45a82a7
Junya Kosaka <kosakajunya@gmail.com>
2022-01-10 15:00:32 +0900

    ...

---
+++ pipex/cmd_full_path.c
@@ -0,0 +1,47 @@
+#include "pipex.h"
+
+static	int	path_index(t_info info)
+{
+	int	index;
+
+	index = 0;
+	while(info.envp[index])
+	{
+			if(!ft_strncmp(info.envp[index], "PATH=", 5))
+					return (index);
+			index++;
+	}
+	return (-1);
+}
+
+void	convert_to_cmd_full_path(t_info *info)
+{
+	char	**all_paths;
+	int		e_index;
+	int		c_index;
+
+	if (!access(info->cmd[0], R_OK))
+	{
+		info->cmd_full_path = info->cmd[0];
+		printf("full path input[%s]\n", info->cmd_full_path);
+		return ;
+	}
+	e_index = path_index(*info);
+	if (e_index == -1)
+		exit(1);
+	all_paths = ft_split(&(info->envp[e_index][5]), ':');
+	c_index = 0;
+	while (all_paths[c_index])
+	{
+
+		info->cmd_full_path = ft_strjoin(all_paths[c_index], "/");
+		info->cmd_full_path = ft_strjoin(info->cmd_full_path, info->cmd[0]);
+		if (!access(info->cmd_full_path, R_OK))
+		{
+			printf("relative path [%s]\n", info->cmd_full_path);
+			return ;
+		}
+		c_index++;
+	}
+	exit(1);
+}
---
+++ pipex/pipex.c
@@ -0,0 +1,122 @@
+#include "pipex.h"
+
+
+
+/* return fd */
+int	get_filefd(t_info info, int i)
+{
+	int	fd;
+
+	if (info.file && i == 2)
+		fd = open(info.file, R_OK);
+	else if (info.file && i + 2 == info.argc)
+		fd = open(info.file, W_OK);
+	else
+		fd = NOFILE;
+	return (fd);
+}
+
+void	dup2_func(t_info info, int filefd, int i)
+{
+	if (filefd != NOFILE && i == 2)
+	{
+		dup2(filefd, 0);
+		dup2(info.pipefd[i - 2][1], 1);
+	}
+	else if (filefd != NOFILE && i + 2 == info.argc) // 最後
+	{
+		// dup2(filefd, 1);
+		dup2(info.pipefd[i - 3][0], 0);
+	}
+	else
+	{
+		dup2(info.pipefd[i - 3][0], 0);
+		dup2(info.pipefd[i - 2][1], 1);
+	}	
+}
+
+void	close_func(t_info info, int filefd, int i)
+{
+	if (filefd != NOFILE && i == 2)
+		close(info.pipefd[i - 2][0]);
+	else if (filefd != NOFILE && i + 2 == info.argc) // 最後
+		close(info.pipefd[i - 3][1]);
+	else
+		close(info.pipefd[i - 3][1]);
+	if (filefd != NOFILE)
+		close(filefd);
+	close(info.pipefd[i - 2][1]);
+}
+
+void	child_exe(t_info info, int i)
+{
+	int	filefd;
+
+	filefd = get_filefd(info, i);
+	if (filefd == -1)
+		exit(1);
+	dup2_func(info, filefd, i);
+	close_func(info, filefd, i);
+	execve(info.cmd_full_path, info.cmd, info.envp);
+	return ;
+}
+
+void	start_process(t_info info)
+{
+	int	wstatus;
+	int	fd;
+	int	i;
+
+	i = 2;
+	while (i + 1 < info.argc)
+	{
+		info.file = NULL;
+		if (i == 2)
+			info.file = info.argv[i - 1];
+		else if (i + 2 == info.argc)
+			info.file = info.argv[i + 1];
+		info.cmd = ft_split(info.argv[i], ' ');
+		convert_to_cmd_full_path(&info);
+
+		if (pipe(info.pipefd[i - 2]) < 0)
+		{
+			perror("pipe");
+			exit(-1);
+		}
+		info.pid = fork();
+		if (info.pid == 0)
+			child_exe(info, i);
+		else {
+			if (i != 2) {
+				close(info.pipefd[i - 3][0]);
+				close(info.pipefd[i - 3][1]);
+			}
+			waitpid(info.pid, &wstatus, WUNTRACED);
+		}
+		printf("\n\n");
+		i++;
+	}
+	exit(0);
+}
+
+int    main(int argc, char **argv, char **envp)
+{
+    t_info    	info;
+	int			i;
+
+    info.argv = argv;
+	info.argc = argc;
+    info.envp = envp;
+	info.pipefd = (int **)malloc(sizeof(int *) * (argc - 3));
+	if (!info.pipefd)
+		exit(1);
+	i = 0;
+	while (i < argc - 3)
+	{
+		info.pipefd[i] = (int *)malloc(sizeof(int) * 2);
+		i++;
+	}
+	start_process(info);
+
+    exit(0);
+}
